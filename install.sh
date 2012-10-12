7a654298810edd88f4f9e3eb8805d50aff1acbd9
Artem V. Andreev <Artem.Andreev@oktetlabs.ru>
2012-10-12 17:08:55 +0400

    typo fixed
    hg-changeset: d54adcc96e0fa67d8d2c01a56ad73cdc9a1a4f56

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-NAMESPACE=root/solarlare
+NAMESPACE=root/solarflare
 PRODUCTNAME=Solarflare
 
 cp lib${PRODUCTNAME}.so ${PEGASUS_HOME}/lib
-regmod -v -n ${NAMESPACE} -c ${PEGASUS_HOME}/lib/lib{PRODUCTNAME}.so
+regmod -v -n ${NAMESPACE} -c ${PEGASUS_HOME}/lib/lib${PRODUCTNAME}.so
