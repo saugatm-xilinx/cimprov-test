9f1813a524f55ebce0d6ba83e827952d8bdc7b42
Marina Maslova <marsik@oktetlabs.ru>
2013-02-11 03:51:50 +0400

    Minor fixes
    hg-changeset: 39d7fb24c42051f595835d358548212a8b1fc958

--- sf_linux.cpp
+++ sf_linux.cpp
@@ -41,6 +41,8 @@
 #include <linux/sockios.h>
 
 #define SYS_PCI_DEVICE_PATH             "/sys/bus/pci/devices"
+#define SYS_SF_MOD_PATH                 "/sys/module/sfc"
+#define SYS_SF_DRV_PATH                 SYS_SF_MOD_PATH "/drivers/pci:sfc"
 #define CLASS_NET_VALUE                 "0x020000"
 #define VENDOR_SF_VALUE                 "0x1924" 
 #define SYS_PATH_MAX_LEN                1024
@@ -107,7 +109,6 @@ namespace solarflare
     bool linuxDeviceCheckPath(const char *path)
     {
         char value[BUF_MAX_LEN];
-        char *port1_path;
 
         if (linuxDeviceGetAttr(path, "class", value, sizeof(value)) < 0)
             return false;
@@ -152,6 +153,30 @@ namespace solarflare
         return 0;
     }
 
+    /**
+     * Perform ioctl command on interface.
+     *
+     * @param ifname            Interface name
+     * @param cmd               Ethtool command
+     * @param ifr               Location for IO data             
+     *
+     * @return zero on success, -1 on error
+     */
+    int linuxIOctlCmd(const char *ifname, unsigned cmd, struct ifreq *ifr)
+    {
+        int sock;
+        
+        sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+        if (sock < 0)
+            return -1;
+
+        strncpy(ifr->ifr_name, ifname, sizeof(ifr->ifr_name));
+        if (ioctl(sock, cmd, ifr) < 0)
+            return -1;
+
+        return 0;
+    }
+
     class LinuxPort : public Port {
         const NIC *owner;
         Interface *boundIface;
@@ -172,7 +197,7 @@ namespace solarflare
         virtual void autoneg(bool an);
         
         /// causes a renegotiation like 'ethtool -r'
-        virtual void renegotiate() {};
+        virtual void renegotiate();
 
         /// @return Manufacturer-supplied MAC address
         virtual MACAddress permanentMAC() const { return MACAddress(0, 1, 2, 3, 4, 5); };
@@ -307,6 +332,15 @@ namespace solarflare
                          ETHTOOL_SSET, &edata);
     }
 
+    void LinuxPort::renegotiate()
+    {
+        struct ethtool_cmd edata;
+
+        memset(&edata, 0, sizeof(edata));
+        linuxEthtoolCmd(boundIface->ifName().c_str(),
+                        ETHTOOL_NWAY_RST, &edata);
+    }
+
     class LinuxInterface : public Interface {
         const NIC *owner;
         Port *boundPort;
@@ -348,56 +382,52 @@ namespace solarflare
 
     bool LinuxInterface::ifStatus() const
     {
-        char buf[BUF_MAX_LEN];
-
-        linuxDeviceGetAttr(sysfsPath.c_str(), "operstate",
-                        buf, sizeof(buf));
-        if (0 == strcmp(buf, "down"))
+        struct ifreq    ifr;
+        
+        memset(&ifr, 0, sizeof(ifr));
+        if (linuxIOctlCmd(ifName().c_str(), SIOCGIFFLAGS, &ifr))
             return false;
-        return true;
+
+        if (ifr.ifr_flags & IFF_UP)
+            return true;
+
+        return false;
     }
 
     void LinuxInterface::enable(bool st)
     {
-        int             sock;
         struct ifreq    ifr;
-        
-        memset(&ifr, 0, sizeof(ifr));
-        sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
-        if (sock < 0)
-            return;
 
-        strncpy(ifr.ifr_name, ifName().c_str(), sizeof(ifr.ifr_name));
-        if (ioctl(sock, SIOCGIFFLAGS, &ifr) <0 )
+        memset(&ifr, 0, sizeof(ifr));
+        if (linuxIOctlCmd(ifName().c_str(), SIOCGIFFLAGS, &ifr))
             return;
+       
         if (st)
             ifr.ifr_flags |= IFF_UP;
         else
             ifr.ifr_flags &= ~IFF_UP;
-        ioctl(sock, SIOCSIFFLAGS, &ifr);
+
+        linuxIOctlCmd(ifName().c_str(), SIOCSIFFLAGS, &ifr);
     }
 
     uint64 LinuxInterface::mtu() const
     { 
-        char buf[BUF_MAX_LEN];
+        struct ifreq    ifr;
 
-        linuxDeviceGetAttr(sysfsPath.c_str(), "mtu", buf, sizeof(buf));
-        return strtoull(buf, NULL, 10);
+        memset(&ifr, 0, sizeof(ifr));
+        if (linuxIOctlCmd(ifName().c_str(), SIOCGIFMTU, &ifr))
+            return 0;
+
+        return ifr.ifr_mtu;
     }
 
     void LinuxInterface::mtu(uint64 u)
     {
-        int             sock;
         struct ifreq    ifr;
-        
-        memset(&ifr, 0, sizeof(ifr));
-        sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
-        if (sock < 0)
-            return;
 
-        strncpy(ifr.ifr_name, ifName().c_str(), sizeof(ifr.ifr_name));
+        memset(&ifr, 0, sizeof(ifr));
         ifr.ifr_mtu = u;
-        ioctl(sock, SIOCSIFMTU, &ifr);
+        linuxIOctlCmd(ifName().c_str(), SIOCSIFMTU, &ifr);
     }
 
     String LinuxInterface::ifName() const
@@ -410,41 +440,33 @@ namespace solarflare
     }
 
     MACAddress LinuxInterface::currentMAC() const
-    { 
-        char            buf[BUF_MAX_LEN];
-        unsigned char   mac[ETH_ALEN];
-        int             i;
-        char           *ptr;
+    {
+        struct ifreq    ifr;
+        
+        memset(&ifr, 0, sizeof(ifr));
+        ifr.ifr_hwaddr.sa_family = ARPHRD_ETHER;
+        if (linuxIOctlCmd(ifName().c_str(), SIOCGIFHWADDR, &ifr) < 0)
+            return MACAddress(0, 0, 0, 0, 0, 0);
 
-        linuxDeviceGetAttr(sysfsPath.c_str(), "address", buf, sizeof(buf));
-        ptr = buf;
-
-        for (i = 0; i < ETH_ALEN; i++)
-        {
-            mac[i] = (unsigned char) strtoul(ptr, &ptr, 16);
-            ptr++;
-        }
-        return MACAddress(mac[0], mac[1], mac[2],
-                          mac[3], mac[4], mac[5]);
+        return MACAddress(ifr.ifr_hwaddr.sa_data[0],
+                          ifr.ifr_hwaddr.sa_data[1],
+                          ifr.ifr_hwaddr.sa_data[2],
+                          ifr.ifr_hwaddr.sa_data[3],
+                          ifr.ifr_hwaddr.sa_data[4],
+                          ifr.ifr_hwaddr.sa_data[5]);
     }
 
     void LinuxInterface::currentMAC(const MACAddress& mac)
     {
-        int             sock;
         int             i;
         struct ifreq    ifr;
         
         memset(&ifr, 0, sizeof(ifr));
-        sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
-        if (sock < 0)
-            return;
-
-        strncpy(ifr.ifr_name, ifName().c_str(), sizeof(ifr.ifr_name));
         ifr.ifr_hwaddr.sa_family = ARPHRD_ETHER;
         for (i = 0; i < ETH_ALEN; i++)
             ifr.ifr_hwaddr.sa_data[i] = mac.address[i];
 
-        ioctl(sock, SIOCSIFHWADDR, &ifr);
+        linuxIOctlCmd(ifName().c_str(), SIOCSIFHWADDR, &ifr);
     }
     
     class LinuxNICFirmware : public NICFirmware {
@@ -462,6 +484,16 @@ namespace solarflare
         virtual void initialize() {};
     };
 
+    VersionInfo LinuxNICFirmware::version() const
+    {
+        struct ethtool_drvinfo edata;
+
+        if (linuxEthtoolCmd(boundIface->ifName().c_str(),
+                             ETHTOOL_GDRVINFO, &edata) < 0)
+            return VersionInfo("");
+
+        return VersionInfo(edata.fw_version);
+    }
 
     class LinuxBootROM : public BootROM {
         const NIC *owner;
@@ -477,18 +509,15 @@ namespace solarflare
 
     class LinuxDiagnostic : public Diagnostic {
         const NIC *owner;
+        const Interface *boundIface;
         Result testPassed;
         static const char sampleDescr[];
         static const String diagGenName;
     public:
-        LinuxDiagnostic(const NIC *o) :
-            Diagnostic(sampleDescr), owner(o), testPassed(NotKnown) {}
-        virtual Result syncTest() 
-        {
-            testPassed = Passed;
-            log().logStatus("passed");
-            return Passed;
-        }
+        LinuxDiagnostic(const NIC *o, const Interface *bi) :
+            Diagnostic(sampleDescr), owner(o), boundIface(bi),
+            testPassed(NotKnown) {}
+        virtual Result syncTest();
         virtual Result result() const { return testPassed; }
         virtual const NIC *nic() const { return owner; }
         virtual void initialize() {};
@@ -497,6 +526,35 @@ namespace solarflare
 
     const char LinuxDiagnostic::sampleDescr[] = "Linux Diagnostic";
     const String LinuxDiagnostic::diagGenName = "Diagnostic";
+
+    Diagnostic::Result LinuxDiagnostic::syncTest() 
+    {
+        struct ethtool_drvinfo  drv_data;
+        struct ethtool_test    *test_data;
+
+        if (linuxEthtoolCmd(boundIface->ifName().c_str(),
+                             ETHTOOL_GDRVINFO, &drv_data) < 0)
+            return NotKnown;
+
+        test_data = (ethtool_test*)calloc(1, sizeof(*test_data) +
+                           drv_data.testinfo_len * sizeof(*test_data->data));
+        if (!test_data)
+            return NotKnown;
+
+        test_data->flags = ETH_TEST_FL_OFFLINE;
+        test_data->len = drv_data.testinfo_len;
+        if (linuxEthtoolCmd(boundIface->ifName().c_str(),
+                            ETHTOOL_TEST, &drv_data) < 0)
+        {
+            free(test_data);
+            return NotKnown;
+        }
+
+        testPassed = test_data->flags & ETH_TEST_FL_FAILED ? Failed : Passed;
+        log().logStatus(testPassed == Passed ? "passed" : "failed");
+        free(test_data);
+        return testPassed;
+    }
 
     class LinuxNIC : public NIC {
         int portNum;
@@ -539,13 +597,13 @@ namespace solarflare
         String sysfsPath;
 
         LinuxNIC(unsigned idx, char *NICPath, int num,
-                  char *ifPath0, char *ifPath1) :
+                 const char *ifPath0, const char *ifPath1) :
             NIC(idx), portNum(num), 
             port0(this, 0), port1(this, 1),
             intf0(this, 0, ifPath0), intf1(this, 1, ifPath1),
             nicFw(this, &intf0),
             rom(this, VersionInfo("2.3.4")),
-            diag(this), sysfsPath(NICPath)
+            diag(this, &intf0), sysfsPath(NICPath)
         {}
 
         virtual VitalProductData vitalProductData() const;
@@ -732,12 +790,84 @@ namespace solarflare
         LinuxDriver(const Package *pkg, const String& d, const String& sn, 
                      const VersionInfo& v) :
             Driver(d, sn), owner(pkg), vers(v) {}
-        virtual VersionInfo version() const { return vers; }
+        virtual VersionInfo version() const;
         virtual void initialize() {};
         virtual bool syncInstall(const char *) { return false; }
         virtual const String& genericName() const { return description(); }
         virtual const Package *package() const { return owner; }
     };
+
+    VersionInfo LinuxDriver::version() const
+    {
+        char                 value[BUF_MAX_LEN];
+        DIR                 *device_dir;
+        struct dirent       *device;
+        char                *iface_name = NULL;
+
+        if (linuxDeviceGetAttr(SYS_SF_MOD_PATH, "version",
+                               value, sizeof(value)) > 0)
+            return VersionInfo(value);
+
+
+        device_dir = opendir(SYS_SF_DRV_PATH);
+        if (device_dir == NULL)
+            return VersionInfo("");
+
+        for (device = readdir(device_dir);
+             device != NULL;
+             device = readdir(device_dir))
+        {
+            char                buf[SYS_PATH_MAX_LEN];
+            struct dirent      *iface;
+            DIR                *net_dir;
+
+            if (device->d_name[0] == '.')
+                continue;
+
+            sprintf(buf, "%s/%s", SYS_SF_DRV_PATH,
+                    device->d_name);
+
+            if (!linuxDeviceCheckPath(buf))
+                continue;
+
+            sprintf(buf + strlen(buf), "%s", "/net");
+
+            net_dir = opendir(buf);
+            if (net_dir == NULL)
+                break;
+
+            for (iface = readdir(net_dir); iface != NULL;
+                 iface = readdir(net_dir))
+            {
+                if (strncmp(iface->d_name, "eth", 3) == 0)
+                {
+                    iface_name = strdup(iface->d_name);
+                    break;
+                }
+            }
+            closedir(net_dir);
+            if (iface_name != NULL)
+                break;
+        }
+        closedir(device_dir);
+
+        if (iface_name != NULL)
+        {
+            struct ethtool_drvinfo edata;
+
+            if (linuxEthtoolCmd(iface_name,
+                                ETHTOOL_GDRVINFO, &edata) < 0)
+            {
+                free(iface_name);
+                return VersionInfo("");
+            }
+
+            free(iface_name);
+            return VersionInfo(edata.version);
+        }
+       
+        return VersionInfo("");
+    }
 
     class LinuxLibrary : public Library {
         const Package *owner;
@@ -897,8 +1027,9 @@ namespace solarflare
                 closedir(net_dir);
             }
 
-            LinuxNIC nic = LinuxNIC(NICNum, port_path[1], pnum,
-                                    iface_path[0], iface_path[1]);
+            LinuxNIC nic = LinuxNIC(NICNum, port_path[0], pnum,
+                                    iface_path[0],
+                                    iface_path[1] ? iface_path[1] : "");
             nic.initialize(); 
             res = en.process(nic);
             ret = ret && res;
@@ -937,16 +1068,4 @@ namespace solarflare
     LinuxSystem LinuxSystem::target;
 
     System& System::target = LinuxSystem::target;
-
-    VersionInfo LinuxNICFirmware::version() const
-    {
-        struct ethtool_drvinfo edata;
-
-        if (linuxEthtoolCmd(boundIface->ifName().c_str(),
-                             ETHTOOL_GDRVINFO, &edata) < 0)
-            return VersionInfo("");
-
-        return VersionInfo(edata.fw_version);
-    }
-
 }
