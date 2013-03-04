250816f61570ac9c1deda46a2a1c99a4cbece918
Marina Maslova <marsik@oktetlabs.ru>
2013-03-04 09:23:27 +0400

    Add README for testsuite and cfg variables
    hg-changeset: 2ab8bb43e7fd8cc26eecbe0c1221b1985964e991

--- testsuite/tester_vars.py
+++ testsuite/tester_vars.py
@@ -1,5 +1,17 @@
-TESTER_HOST= 'http://127.0.0.1:5988'
-TESTER_USER= ''
-TESTER_PASSWORD= ''
-TESTER_NS = 'root/solarflare'
-LOGGER_NAME = 'tester'
+import os
+
+cfg_name = None
+if 'TESTER_CFG' in os.environ:
+    cfg_name = os.environ['TESTER_CFG']
+
+if not cfg_name:
+    cfg_name = 'default'
+
+tester_vars_filename = 'tester_vars_' + cfg_name
+tester_vars_mod = __import__(tester_vars_filename)
+
+TESTER_HOST = tester_vars_mod.TESTER_HOST
+TESTER_USER = tester_vars_mod.TESTER_USER
+TESTER_PASSWORD = tester_vars_mod.TESTER_PASSWORD
+TESTER_NS = tester_vars_mod.TESTER_NS
+LOGGER_NAME = tester_vars_mod.LOGGER_NAME
---
+++ testsuite/tester_vars_ibm.py
@@ -0,0 +1,5 @@
+TESTER_HOST= 'http://127.0.0.1:15988'
+TESTER_USER= 'root'
+TESTER_PASSWORD= 'news@ben'
+TESTER_NS = 'root/solarflare'
+LOGGER_NAME = 'tester'
