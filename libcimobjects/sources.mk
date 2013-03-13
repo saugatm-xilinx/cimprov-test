17b2ce02a77156d292a0dc0a3027613f5a969f7e
Artem V. Andreev <Artem.Andreev@oktetlabs.ru>
2013-03-13 23:30:48 +0400

    bug7535: missing file added
    hg-changeset: 1451b5d8bd6c8e9be3b93b1b4ecfcf5ee3f3b5fc

---
+++ libcimobjects/sources.mk
@@ -0,0 +1,18 @@
+libcimobjects_TARGET = libcimobjects.a
+libcimobjects_DIR = libcimobjects
+libcimobjects_INCLUDES = $(libcimobjects_DIR)
+libcimobjects_DEPENDS = $(CIMPLE_COMPONENTS)
+
+include $(libcimobjects_DIR)/classes.mk
+
+libcimobjects_GENERATED = $(libcimobjects_SOURCES) $(patsubst %.cpp,%.h,$(libcimobjects_SOURCES)) classes.mk
+
+$(eval $(call component,libcimobjects,STATIC_LIBRARIES))
+
+$(libcimobjects_DIR)/.genclass : $(libcimobjects_DIR)/classes $(libcimobjects_DIR)/repository.mof $(CIM_SCHEMA_DIR) $(genclass_TARGET)
+	cd $(libcimobjects_DIR); CIMPLE_MOF_PATH="../$(CIM_SCHEMA_DIR)" $(CURDIR)/$(genclass_TARGET) -S -r -e -F$(notdir $<)
+
+$(filter %.h,$(_libcimobjects_GENERATED)) : $(libcimobjects_DIR)/classes.mk
+
+$(libcimobjects_DIR)/classes.mk : $(libcimobjects_DIR)/.genclass
+	$(AWK) '{ print "libcimobjects_SOURCES += ", $$0 }' $< >$@
