5c08a0f822ac27adcfe36a33a70aa408567cd0fb
Artem V. Andreev <Artem.Andreev@oktetlabs.ru>
2013-03-20 15:08:06 +0400

    bug7535: proper dependency for libcimplecmpi
    hg-changeset: 15b50a90924ee59740394aae0932f574af06a042

--- cimple/cmpi/sources.mk
+++ cimple/cmpi/sources.mk
@@ -6,6 +6,7 @@ libcimplecmpi_SOURCES = CMPI_Containers.cpp \
 libcimplecmpi_TARGET = libcimplecmpi.a
 libcimplecmpi_DIR = cimple/cmpi
 libcimplecmpi_INCLUDES = cmpi cimple/cmpi
+libcimplecmpi_DEPENDS = libcimple
 
 $(eval $(call component,libcimplecmpi,STATIC_LIBRARIES))
 
