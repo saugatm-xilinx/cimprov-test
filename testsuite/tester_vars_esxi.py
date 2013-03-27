a8d98c59214f65be7b2a914660a530cc346e45a9
izbitsky <unknown>
2013-03-27 08:38:28 +0400

    Adding ESXi test configuration; minor fixes to tests
    hg-changeset: 7f0a46761100e7bd530b7bd17945fb8cf40c57c1

---
+++ testsuite/tester_vars_esxi.py
@@ -0,0 +1,7 @@
+TESTER_DESCRIPTION="Configuration for running on ESXi host"
+TESTER_HOST= 'https://127.0.0.1:5989'
+TESTER_USER= 'root'
+TESTER_PASSWORD= 'news@ben'
+TESTER_NS = 'solarflare/cimv2'
+TESTER_INTEROP_NS = 'root/interop'
+LOGGER_NAME = 'tester'
