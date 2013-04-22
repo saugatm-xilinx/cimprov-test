8dd78faad363ca8177d2c4d814afaced178e4686
Artem V. Andreev <Artem.Andreev@oktetlabs.ru>
2013-04-22 17:06:02 +0400

    bug7709: eliminating associations for ESXi
    hg-changeset: 70855d249f280cec0aae4ca6246bc54bd7c37a9f

--- libprovider/SF_ServiceAffectsElement_Provider.cpp
+++ libprovider/SF_ServiceAffectsElement_Provider.cpp
@@ -41,7 +41,9 @@ Enum_Instances_Status SF_ServiceAffectsElement_Provider::enum_instances(
     const SF_ServiceAffectsElement* model,
     Enum_Instances_Handler<SF_ServiceAffectsElement>* handler)
 {
+#if !TARGET_CIM_SERVER_esxi
     solarflare::EnumInstances<SF_ServiceAffectsElement>::allObjects(handler);
+#endif
 
     return ENUM_INSTANCES_OK;
 }
