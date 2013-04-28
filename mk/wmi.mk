6f593a670c3b5f01804da5ce5d48ce8cd5f1f5d6
Artem Andreev <Artem.Andreev@oktetlabs.ru>
2013-04-28 17:05:08 +0400

    fixed DLL export issues
    hg-changeset: 5821920a5669904c7ad1b6ace840f1d7f6bad91e

--- mk/wmi.mk
+++ mk/wmi.mk
@@ -2,7 +2,7 @@ override target_CXX = i686-w64-mingw32-g++
 override target_AR = i686-w64-mingw32-ar 
 override target_CPPFLAGS = -I. -DSF_IMPLEMENTATION_NS=\"$(IMP_NAMESPACE)\" -DSF_INTEROP_NS=\"$(INTEROP_NAMESPACE)\"
 override target_CXXFLAGS = -Wall -W -Wno-unused -Werror -g -fms-extensions -Wno-unknown-pragmas -std=gnu++98
-override target_LDFLAGS = 
+override target_LDFLAGS = -static-libgcc -static-libstdc++ -Wl,--kill-at
 override target_SYSLIBRARIES = ole32 oleaut32 uuid
 
 override CIMPLE_PLATFORM = WIN32_IX86_MSVC
