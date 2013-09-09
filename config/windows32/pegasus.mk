a55844bc278b1259eb748d7635e560df2617f119
Artem V. Andreev <Artem.Andreev@oktetlabs.ru>
2013-09-09 15:26:45 +0400

    bug7972: fixed config for pegasus again
    hg-changeset: 817d79f87b7dbb4d7ef91717abecc75584869d46

--- config/windows32/pegasus.mk
+++ config/windows32/pegasus.mk
@@ -12,4 +12,5 @@ override PEGASUS_PREBUILT = 1
 override PEGASUS_UPSTREAM_VERSION = winbuild
 override PEGASUS_SETUP_SUBDIR =
 override USE_REGMOD = 
-override CIM_SCHEMA_DIR = $(abspath $(CIM_SCHEMA_PATCHDIR))/DMTF
+override CIM_SCHEMA_DIR = $(abspath $(CIM_SCHEMA_PATCHDIR))
+override CIM_SCHEMA_DMTF_SUBDIR = /DMTF
