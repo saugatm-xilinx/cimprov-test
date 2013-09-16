c4e272ce91d2ddeb45929f0231563f242b5cf4da
Artem V. Andreev <Artem.Andreev@oktetlabs.ru>
2013-09-16 21:19:56 +0400

    fixed .spec file
    hg-changeset: 439686d9f59ab1b5cfcf95d3bf101258e54e4dff

--- mk/tog-pegasus.mk
+++ mk/tog-pegasus.mk
@@ -21,5 +21,5 @@ override CIM_INTERFACE=cmpi
 
 override CIM_SCHEMA_DIR = $(abspath $(CIM_SCHEMA_PATCHDIR))
 
-PROVIDER_RPM_BUILD_REQUIRES = tog-pegasus-devel
-PROVIDER_RPM_REQUIRES = tog-pegasus-libs
+PROVIDER_RPM_BUILD_REQUIRES = Build-Requires: tog-pegasus-devel
+PROVIDER_RPM_REQUIRES = Requires: tog-pegasus-libs
