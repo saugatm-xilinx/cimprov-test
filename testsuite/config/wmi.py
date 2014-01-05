c2239ddb7310e5fd76c15c0297f32aacea4f9a68
Marina Maslova <marsik@oktetlabs.ru>
2014-01-05 10:59:22 +0000

    Updating namespace for wmi
    hg-changeset: d2f66d2c990e9536c5d8eb04b737223fec905529

--- testsuite/config/wmi.py
+++ testsuite/config/wmi.py
@@ -3,8 +3,8 @@ from cfg import TesterConfiguration
 TesterConfiguration(
     name=__name__.split('.')[-1],
     desc='Configuration for running on Windows host using WMI client',
-    vendorNS='\\\\root\\cimv2',
-    interopNS='\\\\root\\cimv2',
+    vendorNS='\\\\root\\solarflare',
+    interopNS='\\\\root\\solarflare',
     isWMI=True,
     classListPath = 'classes',
     user='Administrator', passwd='news@ben')
