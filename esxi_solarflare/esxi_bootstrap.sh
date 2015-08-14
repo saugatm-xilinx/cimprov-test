397e4a7132f2a1c6c0cdc5e7c05b4e098fd32c63
Dmitry Izbitsky <izbitsky@oktetlabs.ru>
2015-08-14 11:53:39 +0300

    Updating esxi_bootstrap.sh for newer Workbenches
    hg-changeset: 358b2f9d7e692b67c9b9234b8ca61d1062353aff

--- esxi_solarflare/esxi_bootstrap.sh
+++ esxi_solarflare/esxi_bootstrap.sh
@@ -1,23 +1,55 @@
 #! /bin/sh
 
-CIMPDK_ROOT=/opt/vmware/cimpdk
-CIMPDK_SAMPLES=${CIMPDK_ROOT}/samples
 PRODUCT_NAME=solarflare
 
-set -e
-set -v
+cimpdk_ver=`rpm --queryformat "%{VERSION}" -q vmware-esx-cimpdk-devtools`
+cimpdk_v1=`echo $cimpdk_ver | sed "s/^\([0-9]*\).*/\1/"`
+cimpdk_v2=`echo $cimpdk_ver | sed "s/^[0-9]*[.]\([0-9]*\).*/\1/"`
 
-for sysdir in cim-schema cmpi common hwinfo omc sfcb support; do
-    cp -r ${CIMPDK_SAMPLES}/$sysdir .
-done
-chmod -R +w .
+if test $cimpdk_v1 -le 5 -a $cimpdk_v2 -le 1 ; then
 
-sed -i "s/acme/$PRODUCT_NAME/g" common/visor-oemtgz.sh
+    CIMPDK_ROOT=/opt/vmware/cimpdk
+    CIMPDK_SAMPLES=${CIMPDK_ROOT}/samples
 
-cd solarflare
-set +v 
-source ../common/defs
-set -v
-ln -sf `i686-linux5.0-g++ -print-file-name=libstdc++.a` .
+    set -e
+    set -v
 
-sed -i 's/__cdecl//g' $OPENSOURCE/sfcb/$SFCB_VER/cmpios.h
+    for sysdir in cim-schema cmpi common hwinfo omc sfcb support; do
+        cp -r ${CIMPDK_SAMPLES}/$sysdir .
+    done
+    chmod -R +w .
+
+    sed -i "s/acme/$PRODUCT_NAME/g" common/visor-oemtgz.sh
+
+    cd solarflare
+    set +v 
+    source ../common/defs
+    set -v
+    ln -sf `i686-linux5.0-g++ -print-file-name=libstdc++.a` .
+
+    sed -i 's/__cdecl//g' $OPENSOURCE/sfcb/$SFCB_VER/cmpios.h
+
+else
+
+    CIMPDK_ROOT=`ls /opt/vmware/ | grep cimpdk-$cimpdk_ver`
+    CIMPDK_ROOT="/opt/vmware/$CIMPDK_ROOT"
+    CIMPDK_SAMPLES=${CIMPDK_ROOT}/samples
+
+    cp $CIMPDK_SAMPLES/acme/Makefile .
+
+    sed -i "s/\${PROJDIR}\/src/\${PROJDIR}\/solarflare/g" Makefile
+    sed -i "s/PROV_NAME=acme/PROV_NAME=$PRODUCT_NAME/g" Makefile
+    sed -i "s/PROV_NAMESPACE=acme/PROV_NAMESPACE=$PRODUCT_NAME/g" Makefile
+    sed -i "s/DESC_VENDOR=.*/DESC_VENDOR=Solarflare/g" Makefile
+    sed -i "s/DESC_SUMMARY=.*/DESC_SUMMARY=Solarflare NIC CIM provider/g" Makefile
+    sed -i "s/DESC_DESCRIPTION=.*/DESC_DESCRIPTION=Solarflare NIC CIM provider for VMware ESXi/g" Makefile
+    sed -i "s/DESC_URLS=.*/DESC_URLS=oem\/descriptor-urls.xml/g" Makefile
+    sed -i "s/DESC_DEPENDS=.*/DESC_URLS=oem\/solarflare-vib-depends.xml/g" Makefile
+    sed -i "s/libacmeprovider.so/libsolarflare_nic_provider.so/g" Makefile
+    sed -i "s/\<CFLAGS=\"\(.*\)/CFLAGS=\"-D__USE_XOPEN2K8=1 \1/g" Makefile
+
+    echo "" >>Makefile
+    echo "copy_static_std_cpp:" >>Makefile
+    echo "	\${CP} \`\${CXX} -print-file-name=\"libstdc++.a\"\` \${SRCDIR}" >>Makefile
+    make copy_static_std_cpp
+fi
