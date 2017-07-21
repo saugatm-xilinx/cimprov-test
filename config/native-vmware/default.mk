1d7fd6c5380f4d83c82ef611c5571b1bd99ef2a7
saugat mitra smitra@solarflare.com <unknown>
2017-07-21 12:01:42 +0100

    bug 71942 added flag IS_ESXI set to 1 for esxi_native platform type
    hg-changeset: a873806c4e90603ec42571d33b63404a780fb35b
    hg-branch: native-cimprov-dev2

--- config/native-vmware/default.mk
+++ config/native-vmware/default.mk
@@ -1,16 +1,16 @@
 ##########################################################################
 ##! \file ./config/vmware32/default.mk
 ## <L5_PRIVATE L5_SCRIPT>
-## \author  OktetLabs
-##  \brief  ESXi 5.0 configuration
-##   \date  2013/10/02
+## \author  smitra
+##  \brief  ESXi Native 6.5 configuration
+##   \date  2017/07/20
 ##    \cop  (c) Solarflare Communications Inc.
 ## </L5_PRIVATE>
 ##
 ##########################################################################
 # <example>
 override CIM_SERVER = esxi_native
-#override CIM_SERVER = esxi
+override IS_ESXI = 1
 override CIMPLE_PLATFORM = LINUX_IX86_GNU
 override BITNESS = 32
 override HOST_CIMPLE_PLATFORM = $(CIMPLE_PLATFORM)
