0eb2314ced12856b5e11541689b3a61c2c325e53
saugat mitra smitra@solarflare.com <unknown>
2018-09-14 08:28:12 +0100

    bug81725: Sixpence and crown firmware subtype added in fetch_firmware_image.py
    hg-changeset: 0c9552bf417b577d6f36eac02e8a42a9805dbca5
    hg-branch: native-cimprov-dev2

--- tools/fetch_firmware_image.py
+++ tools/fetch_firmware_image.py
@@ -34,13 +34,15 @@ class FirmwareImageDetails(object):
                              'jericho':34, 'byblos':35, 'groat':36,
                              'shilling':37, 'florin':38, 'threepence':39,
                              'cyclops':40, 'penny':41, 'bob':42,
-                             'hog':43, 'sovereign':44, 'solidus':45}
+                             'hog':43, 'sovereign':44, 'solidus':45,
+                             'sixpence':46, 'crown':47}
         self.bootrom_subtype = {'medford':6, 'medford2':7}
         self.uefirom_subtype = {'medford_x64':0, 'medford_aarch64':1}
         self.firmware_list = ['mcfw-sparta', 'mcfw-thebes', 'mcfw-icarus',
                               'mcfw-jericho', 'mcfw-byblos', 'mcfw-cyclops',
                               'mcfw-shilling', 'mcfw-florin', 'mcfw-bob',
                               'mcfw-hog', 'mcfw-sovereign', 'mcfw-solidus',
+                              'mcfw-sixpence', 'mcfw-crown',
                               'gpxe', 'uefi_medford_x64', 'uefi_medford_aarch64']
 
 
